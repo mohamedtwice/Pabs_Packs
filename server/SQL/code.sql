b63e28eee2a2ec48e4a6007e39a40b59dd8e5e50
Mohamed H. Mohamed <mohamedtwice@gmail.com>
2017-07-24 15:11:52 -0500

    Update code.sql

--- server/SQL/code.sql
+++ server/SQL/code.sql
@@ -37,3 +37,42 @@ CREATE TABLE events (
   packs_made int,
   comments text
 );
+--
+INSERT INTO events (event_date, event_time, event_type, partner_id, packs_promised, packs_made, type, comments)
+VALUES ('Item 1', 2, 30, 20, 'Merch', 'comments for 1')
+
+CREATE TABLE partners (
+  id SERIAL PRIMARY KEY,
+  partner_name varchar(80),
+  partner_address text,
+  partner_phone varchar(80),
+  partner_contact varchar(80)
+);
+
+INSERT INTO partners (partner_name, partner_address, partner_phone, partner_contact)
+VALUES ('Gillete', '123 Main St. Minneapolis, MN 55125', '612-123-1234', 'Joe Schmoe'),
+('Childrens Hospital', '123 Main St. Minneapolis, MN 55125', '612-123-1234', 'Joe Schmoe')
+
+CREATE TABLE event_type (
+  id SERIAL PRIMARY KEY,
+  event_type_name varchar(80)
+  );
+
+INSERT INTO event_type (event_type_name)
+VALUES ('Packing'),
+('Donation'),
+('Donation')
+
+CREATE TABLE vendor (
+  id SERIAL PRIMARY KEY,
+  vendor_name varchar(80),
+  vendor_phone varchar(80),
+  vendor_email varchar(80),
+  vendor_address varchar(80)
+  );
+  
+  INSERT INTO vendor (vendor_name, vendor_phone, vendor_email, vendor_address)
+VALUES ('Vendor 1', '612-123-1234', 'vendor2@vendor.com', '123 Main St. Minneapolis, MN 55125'),
+('Vendor 2', '612-123-1234', 'vendor2@vendor.com', '123 Main St. Minneapolis, MN 55125')
+
+
