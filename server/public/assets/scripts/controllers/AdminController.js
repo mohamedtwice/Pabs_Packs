c275ada5fee12d258714d52880f0dbe0715fd8e0
Carl Page <carlpage@Carls-MacBook-Air.local>
2017-08-09 09:15:27 -0500

    phone number filter working

--- server/public/assets/scripts/controllers/AdminController.js
+++ server/public/assets/scripts/controllers/AdminController.js
@@ -322,25 +322,4 @@ myApp.controller('AdminController', ['EventtypeService', 'PartnerService', 'Vend
     }) // end sweet alert
   }; // end delete
 
-}]).filter('phoneNumber', function() {
-  return function(number) {
-    if (!number) {
-      return '';
-    }
-    number = String(number);
-    number = number.replace(/[^0-9]*/g, '');
-    var formattedNumber = number;
-    var c = (number[0] == '1') ? '1' : '';
-    number = number[0] == '1' ? number.slice(1) : number;
-    var area = number.substring(0, 3);
-    var front = number.substring(3, 6);
-    var end = number.substring(6, 10);
-    if (front) {
-      formattedNumber = (c + "(" + area + ") " + front);
-    }
-    if (end) {
-      formattedNumber += ("-" + end);
-    }
-    return formattedNumber;
-  };
-}); // end controller
+}]); // end controller
