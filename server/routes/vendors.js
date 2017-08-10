abb9f9698242f52f7138036657374e644f7e668a
Carl Page <carlpage@Carls-MacBook-Air.local>
2017-08-09 19:51:32 -0500

    vendors posting from inventory page properly

--- server/routes/vendors.js
+++ server/routes/vendors.js
@@ -47,10 +47,10 @@ router.get('/', function(req, res) {
 // POST newVendor
 router.post('/', function(req, res) {
   console.log(req.body);
-  var name = req.body.vendor_name;
-  var phone = req.body.vendor_phone;
-  var email = req.body.vendor_email;
-  var address = req.body.vendor_address;
+  var name = req.body.name;
+  var phone = req.body.phone;
+  var email = req.body.email;
+  var address = req.body.address;
   // do database query to create a new vendor
   pool.connect()
     .then(function(client) {
